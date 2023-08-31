b998a1c903757b0588e045b64d88534578370261
AppRoach <codeapproach@gmail.com>
2023-08-31 22:31:32 +0200

    Initial Commit

---
+++ Sources/ComplexLayoutTextView/ComplexLayoutTextView.swift
@@ -0,0 +1,6 @@
+public struct ComplexLayoutTextView {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ComplexLayoutTextViewTests/ComplexLayoutTextViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ComplexLayoutTextView
+
+final class ComplexLayoutTextViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ComplexLayoutTextView().text, "Hello, World!")
+    }
+}
